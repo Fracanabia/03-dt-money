26651eda9b38cc5b410c42d884f316a9363ac8bf
Wesley Fracanábia dos Santos <wesleyf.santos92@hotmail.com>
2024-11-18 16:32:01 -0300

    feat: create modal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,19 @@
+import * as Dialog from "@radix-ui/react-dialog"
 import IgniteLogo from "../../assets/ignite-logo.svg"
+import { NewTransactionModal } from "../NewTransactionModal"
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles"
+
 export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={IgniteLogo} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
