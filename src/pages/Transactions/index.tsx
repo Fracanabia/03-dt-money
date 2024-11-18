ad30edca2cab1c8423134ba6b9750794f33bc05a
Wesley Fracanábia dos Santos <wesleyf.santos92@hotmail.com>
2024-11-18 15:36:00 -0300

    feat: create table transactions component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,37 @@
 import { Header } from "../../components/Header"
+import { Summary } from "../../components/Summary"
+import {
+  PriceHightLight,
+  TransactionContainer,
+  TransactionTable,
+} from "./styles"
 
 export const Transactions = () => {
   return (
     <div>
       <Header />
+      <Summary />
+
+      <TransactionContainer>
+        <TransactionTable>
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>
+              <PriceHightLight variant="income">R$ 12.000,00</PriceHightLight>
+            </td>
+            <td>Venda</td>
+            <td>3/04/2022</td>
+          </tr>
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>
+              <PriceHightLight variant="outcome">-R$ 12.000,00</PriceHightLight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   )
 }
