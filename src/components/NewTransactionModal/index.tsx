efd88e81180cbd1abaf46312b7f4cefb8270fc40
Wesley Fracanábia dos Santos <wesleyf.santos92@hotmail.com>
2024-11-19 14:16:51 -0300

    feat: add use-context-selector and scheduler

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,8 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as Dialog from "@radix-ui/react-dialog"
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react"
-import { useContext } from "react"
 import { Controller, useForm } from "react-hook-form"
+import { useContextSelector } from "use-context-selector"
 import * as z from "zod"
 import { TransactionsContext } from "../../contexts/TransactionsContext"
 import {
@@ -23,7 +23,12 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormSchemaInputs = z.infer<typeof newTransactionFormSchema>
 
 export const NewTransactionModal = () => {
-  const { createTransactions } = useContext(TransactionsContext)
+  const createTransactions = useContextSelector(
+    TransactionsContext,
+    context => {
+      return context.createTransactions
+    },
+  )
   const {
     control,
     register,
